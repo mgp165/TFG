f268620936bb398ce4a2a5996c7ebc39a720253e
mgp165 <86433997+mgp165@users.noreply.github.com>
2024-03-02 19:52:10 +0100

    validation instead of test

--- notebooks/utils/classification_metrics.py
+++ notebooks/utils/classification_metrics.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# We are creating a function for save the metrics given by the models as parquets.
-
-# In[1]:
+# We are creating a function for calculating the metrics of the models and saving them.
 
 
 import pandas as pd
 import time
 from sklearn.metrics import roc_auc_score
-
-
-# In[2]:
 
 
 def classification(list_models, list_names, path, X_train, y_train, X_test, y_test, metrics={}):
@@ -27,20 +22,13 @@ def classification(list_models, list_names, path, X_train, y_train, X_test, y_te
 
         metrics[name] = {
             'Train_AUC': roc_auc_score(y_train, train_pred),
-            'Test_AUC': roc_auc_score(y_test, test_pred),
+            'Validation_AUC': roc_auc_score(y_test, test_pred),
             'Run_Time': time.time() - start_time
         }
         
-    metrics = pd.DataFrame.from_dict(metrics, orient='index',columns=['Run_Time', 'Train_AUC', 'Test_AUC'])
-    metrics['delta%'] = 100*(metrics.Test_AUC - metrics.Train_AUC) / metrics.Train_AUC
+    metrics = pd.DataFrame.from_dict(metrics, orient='index',columns=['Run_Time', 'Train_AUC', 'Validation_AUC'])
+    metrics['delta%'] = 100*(metrics.Validation_AUC - metrics.Train_AUC) / metrics.Train_AUC
     
     metrics.to_parquet(path)
     
     return metrics
-
-
-# In[ ]:
-
-
-
-
--- notebooks/utils/regression_metrics.py
+++ notebooks/utils/regression_metrics.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# We are creating a function for saving the metrics given by the models as parquets.
-
-# In[1]:
+# We are creating a function for calculating the metrics of the models and saving them.
 
 
 import pandas as pd
 import time
 from sklearn.metrics import mean_squared_error
-
-
-# In[2]:
 
 
 def regression(list_models, list_names, path, X_train, y_train, X_test, y_test, metrics={}):
@@ -27,20 +22,13 @@ def regression(list_models, list_names, path, X_train, y_train, X_test, y_test, 
 
         metrics[name] = {
             'Train_MSE': mean_squared_error(y_train, train_pred),
-            'Test_MSE': mean_squared_error(y_test, test_pred),
+            'Validation_MSE': mean_squared_error(y_test, test_pred),
             'Run_Time': time.time() - start_time
         }
         
-    metrics = pd.DataFrame.from_dict(metrics, orient='index',columns=['Run_Time', 'Train_MSE', 'Test_MSE'])
-    metrics['delta%'] = 100*(metrics.Test_MSE - metrics.Train_MSE) / metrics.Train_MSE
+    metrics = pd.DataFrame.from_dict(metrics, orient='index',columns=['Run_Time', 'Train_MSE', 'Validation_MSE'])
+    metrics['delta%'] = 100*(metrics.Validation_MSE - metrics.Train_MSE) / metrics.Train_MSE
     
     metrics.to_parquet(path)
     
     return metrics
-
-
-# In[ ]:
-
-
-
-
