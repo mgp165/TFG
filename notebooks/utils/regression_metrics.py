32847f8d191f54badf4ff52f572f8b751a97990a
mgp165 <86433997+mgp165@users.noreply.github.com>
2024-03-05 12:51:37 +0100

    Add files via upload

--- notebooks/utils/regression_metrics.py
+++ notebooks/utils/regression_metrics.py
@@ -3,10 +3,15 @@
 
 # We are creating a function for calculating the metrics of the models and saving them.
 
+# In[1]:
+
 
 import pandas as pd
 import time
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
+
+
+# In[2]:
 
 
 def regression(list_models, list_names, path, X_train, y_train, X_test, y_test, metrics={}):
@@ -21,14 +26,21 @@ def regression(list_models, list_names, path, X_train, y_train, X_test, y_test, 
         test_pred = model.predict(X_test)
 
         metrics[name] = {
-            'Train_MSE': mean_squared_error(y_train, train_pred),
-            'Validation_MSE': mean_squared_error(y_test, test_pred),
+            'Train_MSE': root_mean_squared_error(y_train, train_pred),
+            'Validation_MSE': root_mean_squared_error(y_test, test_pred),
             'Run_Time': time.time() - start_time
         }
         
-    metrics = pd.DataFrame.from_dict(metrics, orient='index',columns=['Run_Time', 'Train_MSE', 'Validation_MSE'])
-    metrics['delta%'] = 100*(metrics.Validation_MSE - metrics.Train_MSE) / metrics.Train_MSE
+    metrics = pd.DataFrame.from_dict(metrics, orient='index',columns=['Run_Time', 'Train_RMSE', 'Validation_RMSE'])
+    metrics['delta%'] = 100*(metrics.Validation_RMSE - metrics.Train_RMSE) / metrics.Train_RMSE
     
     metrics.to_csv(path)
     
     return metrics
+
+
+# In[ ]:
+
+
+
+
